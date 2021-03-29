b2ed12f059ea2284a89f13c0dc743e5f99d6443a
jacob-dumas <jacobdumas@creighton.edu>
2021-03-29 16:47:00 -0500

    Form 3
    Form 3

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,9 +2,21 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+ for (i = 0; i < exercises.length; i++) 
+        selExercises.addItem(exercises[i])
+        
+ for (i = 0; i < coreExercises.length; i++) 
+        selExercises.addItem(coreExercises[i])
 }
 
 
 
 
+
+btnExercises.onclick=function(){
+    results = selExercises.text.includes("situps", "plank")
+    if (results  == true)
+  lblExercises.text = "You chose situps and planks - those are the two core exercises."
+else 
+    lblExercises.text = "You did not pick the two core exercises. "
+}
